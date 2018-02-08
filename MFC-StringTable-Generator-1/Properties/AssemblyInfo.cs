626109fcb78f6d7f6c09f3b14521a59726d64171
pzhlkj6612 <pzhlkj6612@126.com>
2018-02-08 22:15:21 +0800

    Update FileInfo

--- MFC-StringTable-Generator-1/Properties/AssemblyInfo.cs
+++ MFC-StringTable-Generator-1/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MFC-StringTable-Generator-1")]
-[assembly: AssemblyDescription("A generator of MFC's StringTable")]
+[assembly: AssemblyDescription("A generator of MFC's StringTable.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MFC-StringTable-Generator-1")]
